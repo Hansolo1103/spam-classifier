df23adfcd88733325a8766c67452613981a45472
Dev Bhandari <51533301+D-z-V@users.noreply.github.com>
2022-04-07 11:17:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def home_page():
     return render_template("index.html", form=form, status=status)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
